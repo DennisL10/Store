afe17b390f9f77ef9f13f033ee2b69277e64030b
DennisL10 <fl0368032019@unab.edu.sv>
2021-11-16 23:41:03 -0600

    TestMenu

--- app/src/main/java/com/dennis/store/MainActivity.kt
+++ app/src/main/java/com/dennis/store/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         btnRegister = findViewById(R.id.btnRegister)
         btnLogin = findViewById(R.id.btnLogin)
-        et_User = findViewById(R.id.et_User)
+        et_User = findViewById(R.id.et_Nombre)
         et_Pass = findViewById(R.id.et_Pass)
 
 
--- app/src/main/java/com/dennis/store/SesionIniciada.kt
+++ app/src/main/java/com/dennis/store/SesionIniciada.kt
@@ -1,11 +1,18 @@
 package com.dennis.store
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SesionIniciada : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sesion_iniciada)
+    }
+    fun Menu(Vista: View){
+        val menu = Intent(this, MenuCalzado::class.java)
+        startActivity(menu)
+
     }
 }
