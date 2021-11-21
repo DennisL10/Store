b65074975695629a7a9aa9cee8ae35bd10c88217
Skevin07 <63690856+Skevin07@users.noreply.github.com>
2021-11-21 16:34:34 -0600

    Agregando nuevas funciones

--- app/src/main/java/com/dennis/store/MenuCalzado.kt
+++ app/src/main/java/com/dennis/store/MenuCalzado.kt
@@ -1,11 +1,19 @@
 package com.dennis.store
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MenuCalzado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_calzado)
+    }
+
+    fun Info(Vista: View){
+        val Info = Intent(this, ActivityZapato1::class.java)
+        startActivity(Info)
+
     }
 }
