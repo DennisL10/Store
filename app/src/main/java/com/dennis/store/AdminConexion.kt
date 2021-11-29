02e631cd0b66f72d42e80f554b6c55e5de7ccd6b
DennisL10 <fl0368032019@unab.edu.sv>
2021-11-28 22:23:29 -0600

    Conexion

---
+++ app/src/main/java/com/dennis/store/AdminConexion.kt
@@ -0,0 +1,21 @@
+package com.dennis.store
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class AdminConexion (
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+    ) : SQLiteOpenHelper(context, name, factory, version) {
+        override fun onCreate(UsuConexion: SQLiteDatabase?) {
+            UsuConexion?.execSQL("Create table usuario(id int primary key, Nombre text, Apellido text, Direccion text, DireccionE text, Usuario text, Contra text)")
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            TODO("Not yet implemented")
+        }
+}
+
